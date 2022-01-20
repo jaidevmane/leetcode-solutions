08c7cf0284a6292ed24cbfb87835fcf801696ee7
Jai Subash Devmane <devmane.j@northeastern.edu>
2022-01-19 23:12:34 -0500

    Time: 1 ms (86.80%), Space: 35.9 MB (82.96%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<Integer>();
+        while(n != 1 && !set.contains(n)){
+            set.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+        
+        
+        
+    }
+    
+    private int getNext(int n){
+        int totalSum = 0;
+        while(n > 0){
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+}
