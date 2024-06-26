31b093188d6287feb8c372736fb60ff5f7878df0
Jai Subash Devmane <devmane.j@northeastern.edu>
2024-06-25 19:20:56 -0700

    Time: 89 ms (62.27%), Space: 18.1 MB (98.03%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -10,6 +10,7 @@ class Solution:
                 res = parent[res]
             return res
 
+
         def union(n1, n2):
             p1, p2 = find(n1), find(n2)
 
@@ -20,14 +21,13 @@ class Solution:
                 parent[p1] = p2
                 rank[p2] += rank[p1]
             else:
-                parent[p2] = p1
+                parent[p2] = parent[p1]
                 rank[p1] += rank[p2]
             return 1
-        
-        res  = n
+
+
+        res = n
         for n1, n2 in edges:
             res -= union(n1, n2)
         return res
-
-
         
