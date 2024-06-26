8416fe9bce22bf866e002b6a90969c415ab400f7
Jai Subash Devmane <devmane.j@northeastern.edu>
2024-06-25 23:50:31 -0700

    Time: 51 ms (80.4%), Space: 16.8 MB (95.68%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parent = [i for i in range(len(edges) + 1)]
+        rank = [1] * (len(edges) + 1)
+
+        def find(n):
+            res = parent[n]
+            while res != parent[res]:
+                parent[res] = parent[parent[res]]
+                res = parent[res]
+            return res      
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+            
+            if rank[p2] > rank[p1]:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            else:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            return True
+        
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return [n1, n2]
