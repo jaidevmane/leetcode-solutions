3f0d853869deba381869fd0b2f721fb3a48abfe7
Jai Subash Devmane <devmane.j@northeastern.edu>
2024-06-26 17:30:47 -0700

    Time: 39 ms (34.53%), Space: 16.6 MB (29.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -6,8 +6,7 @@ class Solution:
             return nums[0]
         if n == 1:
             return max(nums[0], nums[1])
-        
-        memo = {0: nums[0], 1:max(nums[0], nums[1]) }
+        memo = {0: nums[0], 1: max(nums[0], nums[1])}
 
         def helper(i):
             if i in memo:
@@ -15,6 +14,5 @@ class Solution:
             else:
                 memo[i] = max(nums[i] + helper(i - 2), helper(i - 1))
                 return memo[i]
-
-        return helper(n)        
+        return helper(n)
         
